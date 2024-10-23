c552d8745c2c0038314b504c080deec43b0b55fc
顾北 <3554578755@qq.com>
2024-10-23 14:12:57 +0800

    生成一个简单的计算器 输出特定数字

--- twenty_two.py
+++ twenty_two.py
@@ -1,25 +1,25 @@
 # 一个最简单的计算，支持+, -, *, / 四种运算。仅需考虑输入输出为整数的情况(除法结果就是商，忽略余数)
-C=input("请输入运算符号：")
-a=int(input("输入第一位数："))
-b=int(input("输入第二位数："))
-if C=='+':
-    print(f"{a}+{b}={a+b}")
-elif C=='-':
-    print(f"{a}-{b}={a-b}")
-elif C=='*':
-    print(f"{a}*{b}={a*b}")
-elif C=='/':
-    print(f"{a}/{b}={a/b}")
-
+def com(a:int,b:int,C:str) :
+    if C=='+':
+        print(f"{a}+{b}={a+b}")
+    elif C=='-':
+        print(f"{a}-{b}={a-b}")
+    elif C=='*':
+        print(f"{a}*{b}={a*b}")
+    elif C=='/':
+        print(f"{a}/{b}={a/b}")
+com(5,6,'+')
 # 有1,2,3,4,个数字，能组成多少个互不相同且无重复数字的三位数？都是多少？
-count=0
-for i in range(1,5):
-    for q in range(1,5):
-        for w in range(1,5):
-            if i!=q and i!=w and q!=w:
-                count+=1
-                print(i,q,w)
-print(f"有{count}个")
+def num():
+    count=0
+    for i in range(1,5):
+        for q in range(1,5):
+            for w in range(1,5):
+                if i!=q and i!=w and q!=w:
+                    count+=1
+                    print(i,q,w)
+    print(f"有{count}个")
+num()
 
 
 
